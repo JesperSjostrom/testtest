da3a6b14d804a85e0f3a3eb95a8c733dca893ece
Jesper Sjostrom <jespersjostrom2@gmail.com>
2020-10-12 10:58:24 +0200

    test

---
+++ public/javascripts/main.js
@@ -0,0 +1,30 @@
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    let inputCityValue = document.getElementById("txtCity").value;
+  
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Stockholm&appid=be87d6d3c69e84700a081f270920912c&units=metric`)
+     .then(response => response.json())
+     .then(data => {
+        const { main, name, sys, weather } = data;
+  
+        const li = document.createElement("li");
+        li.classList.add("city");
+  
+        const markup = `
+          <div>
+              <h2>
+                  <span>${name}</span>
+                  <sup>${sys.country}</sup>
+              </h2>
+              <div>${main.temp}<sup>°C</sup></div>
+          </div>
+        `;
+  
+        li.innerHTML = markup;
+        document.getElementById("cities").appendChild(li);
+      })
+     .catch(() => {
+      console.log("something went wrong, sorry");
+    });
+  
+  });
